904a98dc20f31ea8167f6c9eeea4bbba8defff41
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2017-10-25 11:45:33 -0400

    1st attempted run of five circles (no work)

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,8 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    var dx : Int
+    
     // This function runs once
     override init() {
         
@@ -16,19 +18,31 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
+        dx = 1
+        
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
+ 
+        canvas.fillColor = Color.init(hue: x, saturation: 100, brightness: 100, alpha: 100)
         
         // Change position
-        x += 1
+        x += dx
+        //Check position and reverse course if  we go off the edge of the screen
+        dx = -1
         
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        
+        canvas.drawEllipse(centreX: dx, centreY: 350, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
+        canvas.drawEllipse(centreX: dx, centreY: 550, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: 650, width: 50, height: 50)
+        canvas.drawEllipse(centreX: dx, centreY: 150, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
+
     }
-    
 }
+
