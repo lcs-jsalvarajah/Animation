2ce3a55a6ae505ccbbfb82a548d9476c86980f1a
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2017-10-27 14:12:42 -0400

    Working version of bouncing circle!!!!

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,8 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     var dx : Int
+    var y : Int
+    var dy : Int
     
     // This function runs once
     override init() {
@@ -16,9 +18,13 @@ class Sketch : NSObject {
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
+        // Random value
+        let randomValue = random(from: 1, toButNotIncluding: 50)
         // Set starting position
         x = 0
         dx = 1
+        y = randomValue
+        dy = 1
         
     }
     
@@ -30,6 +36,7 @@ class Sketch : NSObject {
         
         // Change position
         x += dx
+        y += dy
         
         //Check position and reverse course if  we go off the edge of the screen
         if x > 475 {
@@ -38,9 +45,15 @@ class Sketch : NSObject {
         if x < 25 {
             dx = 3
         }
+        if y > 475 {
+            dy = -3
+        }
         
+        if y < 25 {
+            dy = 3
+        }
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: x, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
 
 }
 }
