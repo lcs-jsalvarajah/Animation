4426f2356dcaffe6275e2d42873593ef3efabfd0
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2017-10-27 13:52:15 -0400

    The working version of the 4 circles challenge!

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,7 +9,7 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
-    var dx : Int
+    
     
     // This function runs once
     override init() {
@@ -18,11 +18,9 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 0
-        dx = 1
-        
-        
-    }
+        x = 250
+       
+        }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
@@ -30,18 +28,24 @@ class Sketch : NSObject {
         canvas.fillColor = Color.init(hue: x, saturation: 100, brightness: 100, alpha: 100)
         
         // Change position
-        x += dx
-        //Check position and reverse course if  we go off the edge of the screen
-        dx = -1
+        x += 1
+       
         
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        canvas.drawEllipse(centreX: dx, centreY: 350, width: 50, height: 50)
+        
+          //Draw an elipse in the middle (moving left)
+        canvas.drawEllipse(centreX: 500-x, centreY: 350, width: 50, height: 50)
+        
+        // Draw an ellipse in the top of the canvas
         canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
-        canvas.drawEllipse(centreX: dx, centreY: 550, width: 50, height: 50)
-        canvas.drawEllipse(centreX: x, centreY: 650, width: 50, height: 50)
-        canvas.drawEllipse(centreX: dx, centreY: 150, width: 50, height: 50)
+        
+        // Draw an ellipse in the bottom of the canvas
         canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
+        
+        // Draw an ellipse in the bottom of the canvas (moving left)
+        canvas.drawEllipse(centreX: 500-x, centreY: 150, width: 50, height: 50)
+
 
     }
 }
