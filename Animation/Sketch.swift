e7f3d1b0e0acbf59acad037d7ba5e8bead37caab
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2017-11-02 14:08:39 -0400

    Beautiful Colourful Canonball

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,9 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var dx : Int
+    var y : Int
+    var dy : Int
     
     // This function runs once
     override init() {
@@ -15,20 +18,34 @@ class Sketch : NSObject {
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
+        // Random value
+       
         // Set starting position
-        x = 250
+        x = 0
+        dx = 1
+        y = 0
+
+        dy = 1
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
-        
+      
+       
+        canvas.fillColor = Color.init(hue: x, saturation: 100, brightness: 100, alpha: 100)
+        canvas.drawShapesWithBorders = false
         // Change position
-        x += 1
+        x += dx
+        y = Int(-0.01*pow(Double(x)-150, 2) + 450)
+        print(y)
+        
+        //Check position and reverse course if  we go off the edge of the screen
+       
+        
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
         
     }
-    
 }
